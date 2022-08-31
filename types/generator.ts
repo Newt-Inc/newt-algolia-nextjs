a2785d5b4451d8a24c89c3aece97540d9de2f4e7
Yohei <y.meguro.2008@gmail.com>
2022-08-31 18:13:08 +0900

    chore: add types

---
+++ types/generator.ts
@@ -0,0 +1,21 @@
+import type { Content } from 'newt-client-js'
+
+interface Image {
+  _id: string
+  src: string
+  fileType: string
+  fileName: string
+  fileSize: number
+  width: number
+  height: number
+}
+
+export interface Generator extends Content {
+  title: string
+  logo: Image
+  description?: string
+  url: string
+  language: string
+  templates: string[]
+  star: number
+}
