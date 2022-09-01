f463f91107e976ea997faacf1d4a4e79d27e6a0d
Yohei <y.meguro.2008@gmail.com>
2022-09-01 12:06:49 +0900

    fix: Hitを修正

--- components/Hit.tsx
+++ components/Hit.tsx
@@ -1,16 +1,25 @@
-import Link from 'next/link'
 import { Highlight } from 'react-instantsearch-hooks-web'
 
 export function Hit({ hit }: any): JSX.Element {
   return (
     <div>
-      <Link href={hit.url}>
-        <a href="#">
-          <div>
-            <Highlight attribute="title" hit={hit} />
-          </div>
-        </a>
-      </Link>
+      <img src={hit.logo.src} alt={hit.fileName} width="60" height="60" />
+      <div>
+        <Highlight attribute="title" hit={hit} />
+      </div>
+      <a href={hit.url} rel="noreferrer noopener" target="_blank">
+        {hit.url}
+      </a>
+      <div>
+        <Highlight attribute="description" hit={hit} />
+      </div>
+      <div>
+        <Highlight attribute="language" hit={hit} />
+      </div>
+      <div>
+        <Highlight attribute="templates" hit={hit} />
+      </div>
+      <div>{hit.star}</div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,91 +20,91 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={styles.Header}>
-        <dl>
-          <dt>Newt + Algolia Example</dt>
-          <dd>
-            <a
-              href="https://github.com/Newt-Inc/newt-algolia-nextjs"
-              rel="noreferrer noopener"
-              target="_blank"
-            >
-              GitHub
-            </a>
-            <a href="#" rel="noreferrer noopener" target="_blank">
-              Back to tutorial page
-            </a>
-          </dd>
-        </dl>
-        <h1>Static Site Generators 😉</h1>
-        <InstantSearch
-          indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME + ''}
-          searchClient={searchClient}
-        >
+      <InstantSearch
+        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME + ''}
+        searchClient={searchClient}
+      >
+        <header className={styles.Header}>
+          <dl>
+            <dt>Newt + Algolia Example</dt>
+            <dd>
+              <a
+                href="https://github.com/Newt-Inc/newt-algolia-nextjs"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a href="#" rel="noreferrer noopener" target="_blank">
+                Back to tutorial page
+              </a>
+            </dd>
+          </dl>
+          <h1>Static Site Generators 😉</h1>
           <SearchBox />
-          <Hits hitComponent={Hit} />
-        </InstantSearch>
-      </header>
+        </header>
 
-      <div className={styles.Container}>
-        <nav className={styles.Nav}>
-          <h2>Sort</h2>
-          <select>
-            <option>Name</option>
-            <option>GitHub Stars</option>
-          </select>
-          <h2>Filter</h2>
-          <ul>
-            <li>
-              <label>
-                <input type="checkbox" />
-                JavaScript (2)
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" />
-                PHP (3)
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" />
-                React (12)
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" />
-                Vue (1)
-              </label>
-            </li>
-          </ul>
-        </nav>
+        <div className={styles.Container}>
+          <nav className={styles.Nav}>
+            <h2>Sort</h2>
+            <select>
+              <option>Name</option>
+              <option>GitHub Stars</option>
+            </select>
+            <h2>Filter</h2>
+            <ul>
+              <li>
+                <label>
+                  <input type="checkbox" />
+                  JavaScript (2)
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="checkbox" />
+                  PHP (3)
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="checkbox" />
+                  React (12)
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input type="checkbox" />
+                  Vue (1)
+                </label>
+              </li>
+            </ul>
+          </nav>
 
-        <main className={styles.Main}>
-          Next.js https://www.newt.so/ Production grade React applications that
-          scale. The world’s leading companies use Next.js by Vercel to build
-          static and dynamic websites and web applications. JavaScript React
-          2344
-        </main>
-      </div>
+          <main className={styles.Main}>
+            <Hits hitComponent={Hit} />
+            Next.js https://www.newt.so/ Production grade React applications
+            that scale. The world’s leading companies use Next.js by Vercel to
+            build static and dynamic websites and web applications. JavaScript
+            React 2344
+          </main>
+        </div>
 
-      <footer className={styles.Footer}>
-        <dl>
-          <dt>Newt + Algolia Example</dt>
-          <dd>
-            <a
-              href="https://github.com/Newt-Inc/newt-algolia-nextjs"
-              rel="noreferrer noopener"
-              target="_blank"
-            >
-              GitHub
-            </a>
-            <a href="#">Back to tutorial page</a>
-          </dd>
-        </dl>
-      </footer>
+        <footer className={styles.Footer}>
+          <dl>
+            <dt>Newt + Algolia Example</dt>
+            <dd>
+              <a
+                href="https://github.com/Newt-Inc/newt-algolia-nextjs"
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a href="#">Back to tutorial page</a>
+            </dd>
+          </dl>
+        </footer>
+      </InstantSearch>
 
       <a
         href="https://newt.so/"
